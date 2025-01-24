2567a8a0a33426a046714e7dea29ff5b10c5c65d
harshp0907 <hkp0907@gmail.com>
2025-01-24 12:11:25 +0530

    Add files via upload

---
+++ phy1-1.py
@@ -0,0 +1,140 @@
+'''a=int(input("value 1"))
+b=int(input("value 2"))
+c=a+b
+print("add",c)
+
+a=int(input("value 1"))
+b=int(input("value 2"))
+c=a-b
+print("sub",c) 
+
+
+a=int(input("value 1"))
+b=int(input("value 2"))
+c=a*b
+print("mul",c) 
+
+a=int(input("value 1"))
+b=int(input("value 2"))
+c=a/b
+print("div",c)
+
+a=int(input("value 1"))
+b=int(input("value 2"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("add",c)
+print("sub",d)
+print("mul",e)
+print("div",f)
+
+a=int(input("value 1"))
+b=a*60
+print("hours into min",b)
+
+a=int(input("value 1"))
+b=a/60
+print("min into hours",b)
+
+a=int(input("value 1"))
+b=a*48
+print("dollar into rupes",b)
+
+a=int(input("value 1"))
+b=a/48
+print(b)
+a=int(input("value 1"))
+b=a*48
+print("rupeees ,",b)
+
+a=int(input("grams 1"))
+b=a/1000
+print("wieght",b)
+a=int(input("weight",))
+b=a*1000
+print("grams",b)
+
+
+a=int(input("bytes"))
+b=a/1000
+c=a*0.000001
+d=a/1000000000
+print(b)
+print(c)
+print(d)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
